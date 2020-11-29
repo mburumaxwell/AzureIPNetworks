96e4084bcbdf32315e619ea31158338713b25ddd
Maxwell Weru <mburumaxwell@gmail.com>
2020-11-29 12:07:03 +0300

    Update resource from 20201012 to 20201123

--- src/AzureIPNetworks/AzureIPsHelper.cs
+++ src/AzureIPNetworks/AzureIPsHelper.cs
@@ -21,7 +21,7 @@ namespace AzureIPNetworks
         public static async Task<AzureCloudIpRanges> GetAzureCloudIpsAsync(CancellationToken cancellationToken = default)
         {
             // read the JSON file from embedded resource
-            var resourceName = string.Join(".", typeof(AzureCloudIpRanges).Namespace, "Resources", "ServiceTags_Public_20201012.json");
+            var resourceName = string.Join(".", typeof(AzureCloudIpRanges).Namespace, "Resources", "ServiceTags_Public_20201123.json");
             using (var stream = typeof(AzureCloudIpRanges).Assembly.GetManifestResourceStream(resourceName))
             {
                 // deserialize the JSON file
